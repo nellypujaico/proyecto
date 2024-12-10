44f0805cba562e4bd37a157e0e2977e0e17d11b8
nellypujaico <nellypujaicoq@gmail.com>
2024-12-10 01:12:50 -0500

    primer commit

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+import Primero from './componentes/Primero';
+import Portada from './componentes/Portada';
+
+function App() {
+  return (
+    <div>
+      <Primero />
+      <Portada />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/componentes/Primero.js
@@ -0,0 +1,23 @@
+import React from 'react';
+import { Navbar, Nav } from 'react-bootstrap';
+import './estilo/Primero.css'; // Asegúrate de que el archivo CSS está importado
+
+const Primero = () => {
+  return (
+    <Navbar className="navbar-custom" expand="lg">
+      <Navbar.Brand href="/" style={{padding:' 10px 20px '}}>MagalyCañete</Navbar.Brand>
+      <Navbar.Toggle aria-controls="basic-navbar-nav" />
+      <Navbar.Collapse id="basic-navbar-nav">
+        <Nav>
+          <Nav.Link href="/">Inicio</Nav.Link>
+          <Nav.Link href="/agregar-noticia">Agregar Noticia</Nav.Link>
+          <Nav.Link href="/categoria">Categoria</Nav.Link>
+          <Nav.Link href="/contactanos">Contactanos</Nav.Link>
+          <Nav.Link href="/mi-cuenta">Registrarse</Nav.Link>
+        </Nav>
+      </Navbar.Collapse>
+    </Navbar>
+  );
+};
+
+export default Primero;
